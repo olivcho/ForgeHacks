5c4450f27d75806456baa4fbde3fc6276b40d938
olivcho <olivercho007@gmail.com>
2025-05-26 01:18:07 -0700

    revamp ui w dark mode

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,19 @@ function Hero() {
                 </h1>
             </div>
             <p>Building alone is hard. ForgeHacks is a community-driven hackathon
-                that connects you with makers, mentors, and investors who get it.</p>
-            <CountdownTimer targetDate={hackathonDate} />
+                that connects you with makers, mentors, and investors to help you get it done.</p>
+            <CountdownTimer className="countdown-timer" targetDate={hackathonDate} />
+            <button
+                className="register-button noSelect"
+                onClick={() => window.open("https://lu.ma/event/evt-5L5GDEKngBTgA72", "_blank")}
+            >
+                Secure Your Spot
+                <img
+                    className="arrow-up-right"
+                    src="public/arrow-up-right.svg"
+                    style={{width: "1.7rem", height: "1.7rem"}}/>
+            </button>
+
             <iframe
                 src="https://lu.ma/embed/event/evt-5L5GDEKngBTgA72/simple"
                 className="iframe"
